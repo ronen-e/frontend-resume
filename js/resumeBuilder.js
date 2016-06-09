0a2ea9755087718fcaa5b968b9cbccd008675233
ronen-e <alter.ronen@gmail.com>
2016-06-10 02:18:12 +0300

    Get data from json file disabled caching on page refactor

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,25 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var model, octopus, view;
+function main() {
+     $.getJSON('./data.json').done(initialize);
+ }
+
+ function initialize(data) {
+     console.log('data', data);
+     model = data;
+     var bio = model.bio;
+     var $div = $('<div>');
+     $div.append(
+         [
+         'name: ' + bio.name,
+         'role: ' + bio.role,
+         'email: ' + bio.contacts.email,
+         'skills: ' + bio.skills,
+         'Welcome' + bio.welcomeMsg,
+         'Github: ' + bio.contacts.github].join('<br>')
+     );
+     $div.append('<img src="' + bio.biopic + '"  />');
+     $('body').append($div);
+    //  octopus.init();
+ }
+
+main();
