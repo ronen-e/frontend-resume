d539c576817189d7c663703c98a729af9f4533c4
ronen-e <alter.ronen@gmail.com>
2016-06-20 06:56:52 +0300

    Add uglify and send output to build folder

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,17 @@
 import gulp from 'gulp';
 import browserify from 'browserify';
 import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+import uglify from 'gulp-uglify';
 
-gulp.task('default', () => {
+gulp.task('go', ['browserify'])
+
+gulp.task('browserify', () => {
     return browserify('app/index.js')
         .transform('babelify')
         .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest(''))
+        .pipe(source('bundle.js'))          // gives streaming vinyl file object
+        .pipe(buffer())                     // convert from streaming to buffered vinyl file object
+        .pipe(uglify({ mangle: false }))    // now gulp-uglify works
+        .pipe(gulp.dest('build/js'))
 });
